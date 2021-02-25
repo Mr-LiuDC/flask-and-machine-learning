6cd314ec318b02460f50ce692f7d47d6f42bd56e
刘德财 <442823529@qq.com>
2021-02-25 23:00:19 +0800

    测试mnist训练

--- mnist/mnist_training.py
+++ mnist/mnist_training.py
@@ -38,9 +38,11 @@ model.evaluate(test_images, test_labels, verbose=2)
 '''保存训练得到的模型'''
 model.save("mnist_dense")
 
+'''加载已有的训练模型'''
 mnist_dense = tf.keras.models.load_model("./mnist_dense")
 mnist_dense.summary()
 
+'''识别预测手写数字'''
 forecast = train_images[1].reshape(1, -1)
 res = mnist_dense.predict(forecast)
 print(res)
