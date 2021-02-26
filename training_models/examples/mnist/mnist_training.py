78a48fdef060bd8b18c430736bbbf221e555022b
刘德财 <442823529@qq.com>
2021-02-26 10:51:50 +0800

    调整目录结构

--- training_models/examples/mnist/mnist_training.py
+++ training_models/examples/mnist/mnist_training.py
@@ -16,7 +16,7 @@ for i in range(5):
     plt.subplot(1, 5, i + 1)
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.imshow(train_images[i], cmap="binary")
     plt.xlabel(train_labels[i])
 plt.show()
 
@@ -36,10 +36,10 @@ model.fit(train_images, train_labels, epochs=5)
 model.evaluate(test_images, test_labels, verbose=2)
 
 '''保存训练得到的模型'''
-model.save("mnist_trained_model")
+model.save("../trained_models/mnist_trained_model")
 
 '''加载已有的训练模型'''
-mnist_dense = tf.keras.models.load_model("./mnist_trained_model")
+mnist_dense = tf.keras.models.load_model("../trained_models/mnist_trained_model")
 mnist_dense.summary()
 
 '''识别预测手写数字'''
